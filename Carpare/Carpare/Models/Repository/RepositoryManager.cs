9e7eb5f2eb7195803d6c4b46b33ecb56739064a9
gurko32 <gurko32@gmail.com>
2017-12-10 14:32:12 +0300

    using

--- Carpare/Carpare/Models/Repository/RepositoryManager.cs
+++ Carpare/Carpare/Models/Repository/RepositoryManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using Carpare.Models.Repository;
 namespace Carpare.Models.Repository
 {
     public class RepositoryManager
