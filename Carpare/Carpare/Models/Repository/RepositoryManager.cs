2b440d5213dd24c602531eae408d61639c405df8
gurko32 <gurko32@gmail.com>
2017-12-10 13:47:55 +0300

    RepositoryManager

---
+++ Carpare/Carpare/Models/Repository/RepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Carpare.Models.Repository
+{
+    public class RepositoryManager
+    {
+        public static IRepository Repository { get; set; }
+
+        /*
+         * Create an instance of a concrete Repository and open it. 
+         * The Repository should close itself on shutdown.
+         */
+        static RepositoryManager()
+        {
+            Repository = new SqliteRepository();
+            Repository.Open();
+        }
+    }
+}
