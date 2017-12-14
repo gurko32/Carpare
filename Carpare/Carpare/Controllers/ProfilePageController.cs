f344f176af4a5f0f24206438ee39f9d23c847cf3
crysispeed <kcaglar.guler@tedu.edu.tr>
2017-12-14 16:37:17 +0300

    Changes 2..

--- Carpare/Carpare/Controllers/ProfilePageController.cs
+++ Carpare/Carpare/Controllers/ProfilePageController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Carpare.Models.Entity;
+using Carpare.Models.Transaction;
 
 namespace Carpare.Controllers
 {
@@ -14,6 +16,7 @@ namespace Carpare.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult UpdateUserId(Credentials cr)
         {
