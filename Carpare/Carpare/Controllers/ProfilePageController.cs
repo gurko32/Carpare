4ce42dd00aefe7d402aa3d29d7b59bb71820f60a
crysispeed <kcaglar.guler@tedu.edu.tr>
2017-12-14 16:35:08 +0300

    Changes..

--- Carpare/Carpare/Controllers/ProfilePageController.cs
+++ Carpare/Carpare/Controllers/ProfilePageController.cs
@@ -9,9 +9,15 @@ namespace Carpare.Controllers
     public class ProfilePageController : Controller
     {
         // GET: ProfilePage
+        [HttpGet]
         public ActionResult ProfilePage()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult UpdateUserId(Credentials cr)
+        {
+
         }
     }
 }
